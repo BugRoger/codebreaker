3699fecf8959b3c67717ad2edd752fe1bbe536b5
Michael Schmidt <michael02.schmidt@sap.com>
2011-03-17 22:29:54 +0100

    implement start game feature

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -1,6 +1,12 @@
 module CodeBreaker
   class Game
+    def initialize(output)
+      @output = output
+    end
+
     def start
+      @output.puts 'Welcome to Codebreaker!'
+      @output.puts 'Enter guess:'
     end
   end
 end
