a38f82fa67f960a1534fb01c43e419ce5b4edfba
Michael Schmidt <michael.j.schmidt@gmail.com>
2011-04-07 09:52:33 -0700

    Edited build.sh via GitHub

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 #source "$HOME/.rvm/scripts/rvm"
 #rvm use "1.9.2@codebreaker"
 
-/opt/ruby/bin/bundle install --without osx
+/opt/ruby/bin/bundle install --without=osx
 /opt/ruby/bin/bundle exec cucumber -f junit -o test-reports
